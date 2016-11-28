2de3ef1e9401d0f8861090259ea573dbc661d4e8
Dominic Rakousky <dominic.rakousky@chello.at>
2016-11-28 17:17:28 +0100

    ASP.NET
    Die momentane Website in eine ASP.NET Version umgewandelt. Aber nur von
    dem Wissen was wir bis jetzt haben!

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sharkesh.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Home";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Über Uns";
+
+            return View();
+        }
+
+        public ActionResult Projects()
+        {
+            ViewBag.Message = "Projekte";
+            return View();
+        }
+
+        public ActionResult Offers()
+        {
+            ViewBag.Message = "Angebote";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Kontakt";
+
+            return View();
+        }
+    }
+}
